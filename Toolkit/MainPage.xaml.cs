f3a703960996894f96c9bd4273b0cd6d83f15a02
%username% <%computername%>
2023-10-11 16:22:53 +0800

    Toolkit

---
+++ Toolkit/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Maui.Views;
+namespace Toolkit;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		await Application.Current.MainPage.ShowPopupAsync(new NewPopup());
+	}
+}
+
