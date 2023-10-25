9a6ece504e04a5fa0845c0e6287f497ba209c940
user <win.com>
2023-10-25 15:51:12 +0800

    ANR

---
+++ ANR/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+namespace ANR;
+
+public partial class MainPage : ContentPage
+{
+	public ObservableCollection<Item> ItemList { get; } = [];
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		ItemList.Clear();
+		for(int i = 0; i < 10; i++)
+		{
+			ItemList.Add(new Item());
+		}
+	}
+}
+
+public class Item
+{
+	public string Name { get; set; } = "Item";
+	public bool IsEnable { get; set; }
+	public string Title { get; set; } = "Title";
+
+	public ObservableCollection<SubItem> SubitemList { get; } = [new SubItem(), new SubItem(), new SubItem(), new SubItem(), new SubItem(),];
+}
+
+public class SubItem
+{
+	public string Name { get; set; } = "SubItem";
+	public string Description { get; set; } = "Description";
+}
+
