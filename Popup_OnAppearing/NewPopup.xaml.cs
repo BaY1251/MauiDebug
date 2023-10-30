eecfd5eb7c3303364ee1f3fc8b6e01cb9b7ae625
user <win.com>
2023-10-30 14:26:42 +0800

    Popup

--- Popup_OnAppearing/NewPopup.xaml.cs
+++ Popup_OnAppearing/NewPopup.xaml.cs
@@ -3,21 +3,21 @@ namespace Toolkit;
 
 public partial class NewPopup : Popup
 {
+	private static NewPopup popup = null;
 	public NewPopup()
 	{
 		InitializeComponent();
+		Opened += (s, e) => Task.Delay(800).ContinueWith(s =>
+		{
+			MainThread.BeginInvokeOnMainThread(() => popup.Close());
+		});
 	}
 
 	public async static Task Show()
 	{
-		var popup = new NewPopup();
-		popup.Opened += (s, e) => Task.Delay(800).ContinueWith(s =>
-		{
-			//action?.Invoke();
-			Thread.Sleep(500);
-			MainThread.BeginInvokeOnMainThread(() => popup.Close());
-		});
-
+		popup ??= new NewPopup();
+		await Shell.Current.CurrentPage.ShowPopupAsync(popup);
+		await Shell.Current.CurrentPage.ShowPopupAsync(popup);
 		await Shell.Current.CurrentPage.ShowPopupAsync(popup);
 	}
 }
