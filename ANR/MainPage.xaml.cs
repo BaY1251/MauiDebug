3dae5a7a2db1028f6a6fb102a95a332cdd4474f7
user <win.com>
2024-02-07 08:44:54 +0800

    leak

--- ANR/MainPage.xaml.cs
+++ ANR/MainPage.xaml.cs
@@ -3,7 +3,9 @@ namespace ANR;
 
 public partial class MainPage : ContentPage
 {
-	public ObservableCollection<Item> ItemList { get; set; }
+	private static int Tick = 0;
+	private static int TickCount = 0;
+	public ObservableCollection<Item> ItemList { get; set; } = [];
 
 	public MainPage()
 	{
@@ -14,12 +16,31 @@ public partial class MainPage : ContentPage
 	private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 		var list = new List<Item>();
-		for(int i = 0; i < 1000; i++)
+		for(int i = 0; i < 100; i++)
 		{
 			list.Add(new Item());
 		}
 		ItemList = new(list);
 		OnPropertyChanged(nameof(ItemList));
+	}
+
+	private void Insert_Clicked(object sender, EventArgs e)
+	{
+		Tick = Environment.TickCount;
+		ItemList.Insert(0, new Item() { TickCount = TickCount, Operate = "Insert" });
+		Collection.ScrollTo(0);
+	}
+
+	private void Add_Clicked(object sender, EventArgs e)
+	{
+		Tick = Environment.TickCount;
+		ItemList.Add(new Item() { TickCount = TickCount, Operate = "Add" });
+		Collection.ScrollTo(ItemList.Count - 1);
+	}
+
+	private void Collection_ChildAdded(object sender, ElementEventArgs e)
+	{
+		TickCount = Environment.TickCount - Tick;
 	}
 }
 
@@ -28,7 +49,8 @@ public class Item
 	static int Count = 0;
 	public string Name { get; set; } = $"{Count++}";
 	public bool IsEnable { get; set; }
-	public string Title { get; set; } = "Title";
+	public string Operate { get; set; }
+	public int TickCount { get; set; }
 
 	public ObservableCollection<SubItem> SubitemList { get; } = new ObservableCollection<SubItem> { new(), new(), new(), new(), new() };
 }
