486749ba8b9a5fc52a53f6c6e09065b7f2d5082f
user <win.com>
2024-09-11 16:08:01 +0800

    Issue24710

---
+++ Issue24710/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Issue24710;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Issue24710/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+namespace Issue24710;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		HubConnection hub = new HubConnectionBuilder()
+			.WithUrl("https://localhost:7141/chathub")
+			.AddMessagePackProtocol()
+			.Build();
+
+		try
+		{
+			await hub.StartAsync();
+		}
+		catch(Exception x)
+		{
+			await Shell.Current.DisplayAlert("检索异常", x.Message, "OK");
+		}
+		finally
+		{
+			await hub.StopAsync();
+		}
+	}
+}
+
