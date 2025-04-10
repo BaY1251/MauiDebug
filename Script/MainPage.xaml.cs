bb1505545b4334d122f3c6edaf91afcfdfde2074
user <win.com>
2025-04-10 10:53:41 +0800

    script

---
+++ Script/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CSScriptLib;
+namespace Script;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            dynamic calc = CSScript.Evaluator.LoadCode(
+                """
+                using System;
+                public class Script
+                {
+                    public int Sum(int a, int b)
+                    {
+                        return a+b;
+                    }
+                }
+                """);
+
+            CounterBtn.Text = $"sum = {calc.Sum(1, 2)}";
+        }
+        catch(Exception x)
+        {
+            Shell.Current.DisplayAlert(x.Message, x.StackTrace, "OK");
+        }
+    }
+
+    public interface ICalc
+    {
+        int Sum(int a, int b);
+    }
+}
